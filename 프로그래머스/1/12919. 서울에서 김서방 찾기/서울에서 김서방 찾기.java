4796c5ec86a3e4d9694ff9e944e1e453eef1d536
오정빈 <126961816+LeoOH5@users.noreply.github.com>
2025-11-04 09:27:06 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.82 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int index = 0;
+        for(String s : seoul){
+            if(s.equals("Kim")) break;
+            index++;
+        }
+        answer = "김서방은 " + index + "에 있다";
+        return answer;
+    }
+}
